53ab614b69845b029e9d72e4f1690c57f8af3b7d
Chris W Jones <Christopher.Jones@sourceability.com>
2020-10-16 17:10:58 -0400

    Adds scheduler

---
+++ scheduler/scripts.js
@@ -0,0 +1,73 @@
+(function(){
+  var app = new Vue({
+    el: '#app',
+    data: {
+      teamMembers: ["Chris"],
+      lastPerson: "",
+      newMember: "",
+      order: [0],
+    },
+    mounted() {
+      if (localStorage.teamMembers) {
+        this.teamMembers = JSON.parse(localStorage.teamMembers);
+      }
+      if (localStorage.lastPerson) {
+        this.lastPerson = localStorage.lastPerson;
+      }
+      if (localStorage.order) {
+        this.order = localStorage.order;
+      }
+    },
+    watch: {
+      teamMembers(newMembers) {
+        localStorage.teamMembers = JSON.stringify(newMembers);
+      },
+      lastPerson(newPerson) {
+        localStorage.lastPerson = newPerson;
+      }
+    },
+    methods: {
+      persist() {
+        if (this.newMember === "") {
+          return;
+        }
+        this.teamMembers.push(this.newMember);
+        console.log("Saved new member!");
+        newMemberIndex = this.teamMembers.indexOf(this.newMember);
+        this.order.push(newMemberIndex);
+        console.log("Order: " + JSON.stringify(this.order));
+        this.newMember = "";
+        this.saveState();
+      },
+      moveUp(member) {
+        currentIndex = this.teamMembers.indexOf(member);
+        newIndex = currentIndex - 1;
+        this.move(currentIndex, newIndex);
+      },
+      moveDown(member) {
+        currentIndex = this.teamMembers.indexOf(member);
+        newIndex = currentIndex + 1;
+        this.move(currentIndex, newIndex);
+      },
+      move(fromIndex, toIndex) {
+        this.teamMembers.splice(toIndex, 0, this.teamMembers.splice(fromIndex, 1)[0]);
+        this.saveState();
+      },
+      saveState() {
+        localStorage.teamMembers = JSON.stringify(this.teamMembers);
+        localStorage.lastPerson = this.lastPerson;
+      },
+      advance() {
+        this.lastPerson = this.nextPerson;
+      },
+    },
+    computed: {
+      nextPerson: function() {
+        let lastPersonIndex = this.teamMembers.indexOf(this.lastPerson);
+        let nextPersonIndex = (lastPersonIndex + 1) % this.teamMembers.length
+
+        return this.teamMembers[nextPersonIndex];
+      }
+    },
+  });
+})()
